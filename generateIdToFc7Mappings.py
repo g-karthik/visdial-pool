ddebc4a21625ffd661557202c4cdc0018091b5af
Samyak Datta <datta.samyak@gmail.com>
2017-10-07 20:12:20 -0400

    Saves the coco id to fc7 mappings separately for train and val

--- generateIdToFc7Mappings.py
+++ generateIdToFc7Mappings.py
@@ -2,10 +2,13 @@
 Generate (and save) a mapping from coco image id to fc7 feature vectors
 python generateIdToFc7Mappings.py
 
-Saves the H5 file in : /srv/share/samyak/visdial-pool/data/visdial-v0.9-fc7-features.h5
+Saves the H5 files in :
+-- [train] /srv/share/samyak/visdial-pool/data/visdial-v0.9-fc7-features-train.h5
+-- [val] /srv/share/samyak/visdial-pool/data/visdial-v0.9-fc7-features-val.h5
+
 """
 
-import json
+import json, os
 import argparse
 import h5py as h5
 import numpy as np
@@ -23,9 +26,9 @@ if __name__=='__main__':
 		default="/srv/share/abhshkdz/data/visdial/visdial_params.json"
 	)
 	parser.add_argument(
-		"-outputH5",
+		"-outputDir",
 		help="Output H5 file to save coco image Id to fc7 feature vector",
-		default="/srv/share/samyak/visdial-pool/data/visdial-v0.9-fc7-features.h5"
+		default="/srv/share/samyak/visdial-pool/data/"
 	)
 	args = parser.parse_args()
 
@@ -34,15 +37,21 @@ if __name__=='__main__':
 
 	fc7 = h5.File(args.featuresH5, "r")
 
-	idToFc7 = dict()
+	id_to_fc7_train, id_to_fc7_val = dict(), dict()
 	print ("\nBuilding dict() for train images...")
 	for i in range(len(fc7['images_train'])):
-		idToFc7[train_ids[i]] = np.asarray(fc7['images_train'][i])
+		id_to_fc7_train[train_ids[i]] = np.asarray(fc7['images_train'][i])
 	
 	print ("\nBuilding dict() for val images...")
 	for i in range(len(list(fc7['images_val']))):
-		idToFc7[val_ids[i]] = np.asarray(fc7['images_val'][i])
+		id_to_fc7_val[val_ids[i]] = np.asarray(fc7['images_val'][i])
+
+	train_file_path = os.path.join(args.outputDir, "visdial-v0.9-fc7-features-train.h5")
+	val_file_path = os.path.join(args.outputDir, "visdial-v0.9-fc7-features-val.h5")
+	with h5.File(train_file_path, "w") as f:
+		for id, descr in id_to_fc7_train.items():
+			f.create_dataset(str(id), data=np.asarray(descr))
 	
-	with h5.File(args.outputH5, "w") as f:
-		for id, descr in idToFc7.items():
+	with h5.File(val_file_path, "w") as f:
+		for id, descr in id_to_fc7_val.items():
 			f.create_dataset(str(id), data=np.asarray(descr))
